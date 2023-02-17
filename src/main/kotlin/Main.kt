2082beed3cf7086de0f0a8d828357979228220cb
Ivy-aminga <amingaivy@gmail.com>
2023-02-17 10:49:09 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    val school = "akirachix"
+    println(school[0]+""+school[2]+""+school[3])
+
+    val x = "my name is Ivy"
+    val age = "26 years"
+    val stmt = "Hi my name is $x and I am $age"
+    println(stmt)
+
+    val str = "AdaLab"
+    println(str.length)
+
+    val name = "Ivy"
+    if (name.equals("Ivy"))
+        println("That's me")
+    else
+        println("I don't know who that is")
+
+
+}
+
+
+
+
